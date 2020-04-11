a64f471676fc1cbd2deb15790b5d94c06ed86af8
zackad <zackad.dev@gmail.com>
2020-04-11 10:58:08 +0700

    [refactor] move css file into new directory
    Define render props component

--- pages/_app.js
+++ pages/_app.js
@@ -1,2 +1,8 @@
-import './css/tailwind.src.css'
+import '../styles/tailwind.src.css'
 import '@fortawesome/fontawesome-free/css/all.css'
+
+function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default App
