a1b5f9115db8d6ad78718f9d2ca558dbc17d978d
zackad <zackad.dev@gmail.com>
2020-05-12 11:01:03 +0700

    fix spacing and sizing

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,5 +1,5 @@
 export const Hero = () => (
-  <section className='mt-12 p-2'>
+  <section className='mx-auto mt-12 p-2 max-w-md'>
     <div className='p-3 border rounded-lg'>
       <h2 className='text-6xl font-serif leading-none border-b'>zackad</h2>
       <h2 className='text-lg sm:text-2xl text-gray-400 font-mono leading-loose'>Full stack web developer</h2>
