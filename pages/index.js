2e90ef7380f277c6d099750c15d5cd26be13fc4a
zackad <zackad.dev@gmail.com>
2020-05-15 11:20:20 +0700

    add portofolio section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { Hero } from '../components/Hero'
 import { Skills } from '../components/Skills'
 import { SocialNetworks } from '../components/SocialNetworks'
+import { Portfolio } from '../components/Portfolio'
 
 const Index = () => (
   <div className='bg-gray-900 text-gray-200 font-sans'>
@@ -9,6 +10,7 @@ const Index = () => (
         <Hero />
         <SocialNetworks />
         <Skills />
+        <Portfolio />
       </div>
     </div>
   </div>
