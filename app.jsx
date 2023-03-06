8d3f653ee9e6a324ef11e2147026af60930c5528
zackad <zackad.dev@gmail.com>
2023-03-06 14:47:39 +0700

    chore: add vite entrypoint

---
+++ app.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './pages/index'
+import '@fortawesome/fontawesome-free'
+import './styles/tailwind.src.css'
+
+ReactDOM.render(<App />, document.getElementById('root-app'))
