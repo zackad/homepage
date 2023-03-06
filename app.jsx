3961a58da7d1ff68a560214455d9c28eee048dc9
zackad <zackad.dev@gmail.com>
2023-03-06 15:04:17 +0700

    fix: missing fontawesome css import

--- app.jsx
+++ app.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './pages/index'
-import '@fortawesome/fontawesome-free'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import './styles/tailwind.src.css'
 
 ReactDOM.render(<App />, document.getElementById('root-app'))
